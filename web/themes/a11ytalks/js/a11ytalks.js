8b608c8933e005ca998111311806365799483a75
Mark Casias <mark.casias@kanopi.com>
2023-10-31 16:15:04 -0600

    Inital commit with some theme work

---
+++ web/themes/a11ytalks/js/a11ytalks.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * a11ytalks behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.a11ytalks = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
